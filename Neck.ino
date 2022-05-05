79e2b31388e2980529f23aaae9c76e0a4e78fd77
Disaster of Puppets <62915210+DisasterofPuppets@users.noreply.github.com>
2022-05-06 07:15:34 +1000

    Update Neck.ino

--- Neck.ino
+++ Neck.ino
@@ -2,10 +2,11 @@
 const int dirPin = 2; // Direction
 const int stepPin = 3; // Step
 int motSpeed = 200;  // lower the number faster it goes....i think*
-const int leftPin = 6;
-const int rightPin = 7;
+const int leftPin = 7;
+const int rightPin = 8;
 int sudoPos = 0;
-int runOnce = 1;
+int initial = 1;
+int rHit = 1;
 // Motor steps per rotation
 //const int STEPS_PER_REV = 1600;
 
@@ -18,8 +19,15 @@ pinMode(leftPin,INPUT_PULLUP);
 pinMode(rightPin,INPUT_PULLUP);
 }
 void loop() {
-int Res = digitalRead (rightPin);
-int Les = digitalRead (leftPin);
+
+int Res = digitalRead(rightPin);
+int Les = digitalRead(leftPin);
+//Serial.print("Initial set : ");
+//Serial.println(initial);
+//Serial.print("Left : ");
+//Serial.println(Les);
+//Serial.print("Right : ");
+//Serial.println(Res);
 
 /* Testing default switch state (1 is open confirmed..ie HIGH)
 if (Les == 0){
@@ -36,69 +44,51 @@ digitalWrite(LED_BUILTIN, LOW);
 
 ///////////////////HOMING PROCESS//////////////////
 
-// position stepper in the middle
-if (runOnce == 1){
-
-  while (Les == 1 && Res == 1 or Les == 0 && Res == 1){
-    //run clockwise till you hit the endstop
+if (initial == 1){
+    Serial.println("Moving to Right Endstop");
     digitalWrite(dirPin,HIGH);
-    
-      for(int x = 0; x < 1000 ; x++) {
+
+
+
+    // run until right Endstop is hit (0)
+      for(int x = 0; Res == 1; x++) {
+        Res = digitalRead(rightPin);
         digitalWrite(stepPin,HIGH);
         delayMicroseconds(motSpeed);
         digitalWrite(stepPin,LOW);
         delayMicroseconds(motSpeed);
+
+Serial.print("Right : ");
+Serial.println(Res);
+        
         }
-   }
 
-  while (Res == 0 && Les == 1){ 
-  //it is either already there or the above has run
-  //run anticlockwise till you hit the endstop
-    digitalWrite(dirPin,LOW);
-      
-      for(int x = 0; x < 1000; x++) {
-        digitalWrite(stepPin,HIGH);
-        delayMicroseconds(motSpeed);
-        digitalWrite(stepPin,LOW);
-        delayMicroseconds(motSpeed);
-        int(sudoPos++); //count steps between endstops
-    }
+     // run until left Endstop is hit and count the steps (0)        
+        Serial.println("Moving to Left Endstop");
+        digitalWrite(dirPin,LOW);
 
+        for(int x = 0; Les == 1; x++) {
+          Les = digitalRead(leftPin);
+          Serial.print("Left : ");
+          Serial.println(Les);
+          digitalWrite(stepPin,HIGH);
+          delayMicroseconds(motSpeed);
+          digitalWrite(stepPin,LOW);
+          delayMicroseconds(motSpeed);
+          int(sudoPos++); //count steps between endstops
+          Serial.print("sudoPos : ");
+          Serial.println(sudoPos);
+         }
+       
+       for(int x = 0; x < sudoPos; x++) {
+       // Set Stepper to middle
+          Serial.print("Setting to Middle : ");
+          Serial.println(sudoPos);
+          digitalWrite(dirPin,HIGH);
+          digitalWrite(stepPin,HIGH);
+          delayMicroseconds(motSpeed);
+          digitalWrite(stepPin,LOW);
+          delayMicroseconds(motSpeed);
+          }
   }
-runOnce == 0;
-}
-
-// then go back clockwise sudoPos / 2
-
-///////////////////HOMING PROCESS END/////////////
-
-/*
-
-// Spin motor until endstop hit
-
-  while (Res == 0){
-    
-  }
-    // Set motor direction clockwise
-    digitalWrite(dirPin,HIGH);
-    for(int x = 0; x++) {
-    digitalWrite(stepPin,HIGH);
-    delayMicroseconds(motSpeed);
-    digitalWrite(stepPin,LOW);
-    delayMicroseconds(motSpeed);
-    delay(4000);
-    }
-  }
-  
-  else if (Res == 1);
-  // Set motor direction Counterclockwise
-  digitalWrite(dirPin,LOW);
-
-    for(int x = 0; x < STEPS_PER_REV; x++) {
-    digitalWrite(stepPin,HIGH);
-    delayMicroseconds(motSpeed);
-    digitalWrite(stepPin,LOW);
-    delayMicroseconds(motSpeed);
-}
- */
-}
+}   
