8026d521354622678b114a415297d6429231634e
Disaster of Puppets <62915210+DisasterofPuppets@users.noreply.github.com>
2023-12-22 11:31:52 +1000

    Update GargoyleEsp32Sender.ino

--- GargoyleEsp32Sender.ino
+++ GargoyleEsp32Sender.ino
@@ -8,25 +8,26 @@ const int raspberryPiPort = 8888;
 
 WiFiUDP udp;
 
-#define xPin 13
-#define yPin 14
-#define jbPin 27
-#define mod1Pin 25
-#define mod2Pin 33
-#define mod3Pin 32
-#define mod4Pin 11
 
-int xpos;
-int ypos;
+#define mod1Pin 26
+#define mod2Pin 25
+#define mod3Pin 35
+#define mod4Pin 34
+
 int mod1b;
 int mod2b;
 int mod3b;
 int mod4b;
 
-void setup() {
 
+void setup() {
   
-  Serial.begin(115200);
+  pinMode(mod1Pin,INPUT_PULLUP);
+  pinMode(mod2Pin,INPUT_PULLUP);
+  pinMode(mod3Pin,INPUT_PULLUP);
+  pinMode(mod4Pin,INPUT_PULLUP);
+  
+  Serial.begin(9600);
 
 
 
@@ -39,21 +40,30 @@ void setup() {
 }
 
 void loop() {
+  
 
- // xpos = analogRead(xPin);
-  //ypos = analogRead(yPin);
-//  mod1b = digitalRead(mod1Pin);
-//  mod2b = digitalRead(mod2Pin);
-//  mod3b = digitalRead(mod3Pin);
-//  mod4b = digitalRead(mod4Pin);
+  mod1b = digitalRead(mod1Pin);
+  mod2b = digitalRead(mod2Pin);
+  mod3b = digitalRead(mod3Pin);
+  mod4b = digitalRead(mod4Pin);
 
 //map the joystick position to an angle
   //xpos = map(xpos,0,4095,0,180);
   //ypos = map(ypos,0,4095,0,180); 
 
- // My variables
-String data = String(xpos)+","+String(ypos)+","+String(mod1b)+","+String(mod2b)+","+String(mod3b)+","+String(mod4b);
+Serial.print("Button 1 : ");
+Serial.println(mod1b);
+Serial.print("Button 2 : ");
+Serial.println(mod2b);
+Serial.print("Button 3 : ");
+Serial.println(mod3b);
+Serial.print("Button 4 : ");
+Serial.println(mod4b);
 
+
+ // My variables
+//String data = String(xpos)+","+String(ypos)+","+String(mod1b)+","+String(mod2b)+","+String(mod3b)+","+String(mod4b);
+String data = String(mod1b)+","+String(mod2b)+","+String(mod3b)+","+String(mod4b);
 
   udp.beginPacket(raspberryPiIP, raspberryPiPort);
   udp.print(data);
